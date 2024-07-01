166b2e9a7831a709300d124f616fade8b20b9fc7
luckyseo <ityun777@gmail.com>
2024-07-01 21:49:32 +0900

    ~5.6 tutorials fin

--- users/admin.py
+++ users/admin.py
@@ -3,8 +3,8 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 # Register your models here.
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
+@admin.register(User)#User model을 control할거임
+class CustomUserAdmin(UserAdmin):  #UserModel 상속/ 그냥 admin 패널보다 기능많음 
     fieldsets =(
         (
             "Profile",{
@@ -20,10 +20,10 @@ class CustomUserAdmin(UserAdmin):
         (
             'Important Dates',{
                 'fields': ('last_login', 'date_joined'),
-                'classes':('collapse',),
+                'classes':('collapse',), #hidden
                 },
         )
     )
     list_display = (
         'username','email','name','is_host',
-    ) #모델 안에 존재하는 것들이여야함!!!!
+    ) #뭐가 보일지 결정 +model에 있는것들이여야함
--- users/models.py
+++ users/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-#models.Models안함 : 장고 user다 상속받을라고 , 처음부터X
+#models.Models안함 - why? : 장고 user다 상속받을라고 , 처음부터X
 class User(AbstractUser):
     first_name=models.CharField(max_length=150,editable=False)
-    last_name=models.CharField(max_length=150, editable=False)
+    last_name=models.CharField(max_length=150, editable=False) #editable - show on admin panel?
     name = models.CharField(max_length=150,default="")
     is_host = models.BooleanField(default=False)#null=True: existing users :NULL
+    
