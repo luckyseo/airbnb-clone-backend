6fa6e37633c24f387af4e67002cd35e500fe5991
luckyseo <ityun777@gmail.com>
2024-12-16 12:17:52 +1100

    user configured

--- users/models.py
+++ users/models.py
@@ -3,10 +3,39 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-#models.Models안함 - why? : 장고 user다 상속받을라고 , 처음부터X
+# models.Models안함 - why? : 장고 user다 상속받을라고 , 처음부터X
 class User(AbstractUser):
-    first_name=models.CharField(max_length=150,editable=False)
-    last_name=models.CharField(max_length=150, editable=False) #editable - show on admin panel?
-    name = models.CharField(max_length=150,default="")
-    is_host = models.BooleanField(default=False)#null=True: existing users :NULL
-    
+
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean Won"
+        USD = "usd", "Dollar"
+
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(
+        max_length=150, editable=False
+    )  # editable - show on admin panel?
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)  # null=True: existing users :NULL
+
+    avatar = models.ImageField(blank=True)
+    #cmd poetry add Pillow
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices
+        )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices
+        )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices
+        )
